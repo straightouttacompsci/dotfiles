6d9ff36c1e67ce853de4832aa3bdf5cbdaca6dcf
straightouttacompsci <straightouttacompsci@protonmail.com>
2019-12-01 00:19:32 +0000

    Added TODOS and comments to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
 sudo apt -y update && apt upgrade
+#Reasons for not obvious things:
+#exfat-fuse and exfat-utils are for mounting my ExFat USB that I created in Windows
+#xserver-xorg is required for lightdm or any GUI really.
 sudo apt -y install i3 vim git zathura youtube-dl lightdm xserver-xorg firefox-esr exfat-fuse exfat-utils
